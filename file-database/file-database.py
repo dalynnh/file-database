698c4dd8cbdce5b09361f39556f44c6daf5e76c7
Dalynn Hatch <dalynn.hatch@outlook.com>
2020-03-03 00:34:53 -0600

    added get record by id

--- file-database/file-database.py
+++ file-database/file-database.py
@@ -19,30 +19,69 @@ class Database():
                 wf.write('\n')
                 num_records += 1
 
-        with open(name + '.config', 'w') as f:
-            record_size = 0
-            for field_name, field_value in fields.items():
-                f.write(field_name + ',' + str(field_value) + '\n')
-                record_size += field_value
-            f.write('record_size,' + str(record_size + 1) + '\n')
-            f.write('num_records,' + str(num_records) + '\n')
-
         with open(name + '.overflow', 'w') as _:
             pass
 
         self.name = name
         self.fields = fields
-        self.record_size = record_size
         self.num_records = num_records
+        self.record_size = 1
+        for value in fields.values():
+            self.record_size += value
+
+    def get_record_by_id(self, i: int) -> Dict[str, int] or str:
+        return (
+            self._binary_search(i)
+            or self._linear_search(i)
+            or 'Record could not be found'
+        )
+
+    def _binary_search(self, i) -> Dict[str, int]:
+        with open(self.name + '.data', 'r') as f:
+            low = 0
+            high = self.num_records - 1
+            record = ""
+            while high >= low:
+                middle = int((low + high) / 2)
+                f.seek(middle * self.record_size)
+                record = f.readline()
+                key = next(iter(self.fields))
+                data_id = int(
+                    record[:self.fields[key]]
+                    .replace("-", "").lower()
+                )
+                if data_id == i:
+                    return self._str_to_dict(record)
+                elif data_id < i:
+                    low = middle + 1
+                else:
+                    high = middle - 1
+            return None
+
+    def _linear_search(self, i) -> Dict[str, int]:
+        with open(self.name + '.overflow', 'r') as f:
+            for line in f.readlines():
+                data_id = int(
+                    line[:next(iter(self.fields.values))]
+                    .replace("-", "").lower()
+                )
+                if data_id == i:
+                    return self._str_to_dict(line)
+            return None
+
+    def _str_to_dict(self, record) -> Dict[str, int]:
+        result = {}
+        pos = 0
+        for key, value in self.fields.items():
+            result[key] = record[pos: value + pos].replace('-', '')
+            pos += value
+        return result
 
 
 if __name__ == '__main__':
     test_data_fields = {
-        'rank': 4,
-        'name': 35,
-        'city': 20,
-        'state': 2,
-        'zip': 5,
-        'employee': 10
+        'id': 2,
+        'name': 6
     }
     database = Database('test_data', test_data_fields)
+    print(database.get_record_by_id(12))
